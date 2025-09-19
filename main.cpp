92dcf83fa133d189b01cb02c98e2b5565df0f974
nowayy3 <popov.stepangh@gmail.com>
2025-09-19 17:00:31 +0300

    fixed bugs

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+main(){
+// enum EMode {
+//     kF2C,
+//     kC2F
+// };
+
+
+// EMode Parsemode(char* ){
+//     if (strcmp(mode, "f2c") == 0){
+//         std::cout << ""
+//     }
+//     if (strcmp(mode, "c2f") == 0){
+
+//     }
+// }
+
+// int main(int argc, char** argv){
+//    if (argc != 3){
+//     std::cout << "Usage: ./degree_converter <mode> <degree>\n" 
+//         << "\t<mode>\t\t-- f2c or c2f\n"
+//         << "\t<degree>\t-- degrees in double"
+//         << std::endl;
+//         return 1;
+//    }
+//    double degrees = std::stod(argv[2])
+//    char* mode * argv[1];
+//    if (strcmp(mode, "f2c") == 0){
+//         std::cout << (fahrenheit - 32) * 5 / 9 << std::endl;
+//    }else if (strcmp(mode, "c2f") == 0){
+//     std::cout << "F = " << celsius * 9 /5 + 32 << std::endl; 
+//    }else{
+//     std::cout << "Unrecognized mode: " << mode << std::endl;
+//     return 1;
+//    }
+    std::cout << 123;
+    return 0;
+}
